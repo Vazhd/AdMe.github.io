a15761dcb1ba735fe8627ed94a60f657346617a7
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2022-05-09 17:19:14 +0600

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const {Schema} = require('mongoose')
+const {model} = require('mongoose')
+
+const User = new Schema({
+    username:{type:String, unique:true, required:true},
+    password:{type:String, required:true},
+    roles:[{type:String, ref:'Role'}]
+})
+
+module.exports = model('User', User)
