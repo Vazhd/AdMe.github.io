9cfd4f8c4b5f23f4abf37a77df58cd01c05f631e
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2022-05-26 23:14:04 +0600

    Delete config directory

--- config/database.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //url: 'mongodb://localhost:27017/wednesday'
-    url:'mongodb+srv://beisembiyev:baha91290@cluster0.lxq92.mongodb.net/wednesday?retryWrites=true&w=majority'
-}
