f388decf8fe2af6b33f2f332f4c3279bdd71c92b
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2022-05-27 21:37:29 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express()
 const port = process.env.PORT||3000;
 const MONGO_URL="mongodb+srv://Merey:06012004@cluster0.am5px.mongodb.net/?retryWrites=true&w=majority"
 const swaggerUi = require("swagger-ui-express"),
-swaggerDocument = require("./swagger.json");
+    swaggerDocument = require("./swagger.json");
+const path = require("path");
 mongoose.connect( MONGO_URL, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
@@ -16,11 +17,15 @@ mongoose.connect( MONGO_URL, {
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
-
+app.use(express.static(path.resolve(__dirname,'views')))
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
 })
+app.get('/about', (req, res) => {
+  res.render('about.ejs',{title:'About Us'})
+})
+
 
 app.use('/articles', articleRouter)
 
